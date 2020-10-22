3f983a866b2d015f535e604a339e504650beb6c2
Birajit <birajit95@gmail.com>
2020-10-22 10:10:27 +0530

    Add: getCoinStatus() is added to flip the coin

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,3 +1,12 @@
 #! /bin/bash -x
 
 echo 'Welcome to Flip Coin Combination Program'
+
+getCoinStatus(){
+    if [ $(( RANDOM%2 )) -eq 1 ]
+    then
+        echo "Head"
+    else
+        echo "Tail" 
+    fi       
+}
